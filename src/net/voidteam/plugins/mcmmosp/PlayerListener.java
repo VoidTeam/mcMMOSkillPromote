9af5cecf3ae4c1c5a04f543dc5e5e6c83f583b34
Jack Klink <jacklinkproductions@mac.com>
2013-07-04 17:44:01 -0400

    It works!
    It now will announce the skills for which you are eligible every time
    you skill up. It will add you to a group if you arent already in it.

--- src/net/voidteam/plugins/mcmmosp/PlayerListener.java
+++ src/net/voidteam/plugins/mcmmosp/PlayerListener.java
@@ -5,12 +5,16 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scoreboard.Scoreboard;
 
+import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionManager;
 import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 import com.gmail.nossr50.datatypes.player.McMMOPlayer;
+import com.gmail.nossr50.datatypes.player.PlayerProfile;
+import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.events.experience.McMMOPlayerLevelUpEvent;
 import com.gmail.nossr50.util.player.UserManager;
 
@@ -42,20 +46,58 @@ public class PlayerListener implements Listener
 		PermissionManager pex = PermissionsEx.getPermissionManager();
 		PermissionUser peplayer = pex.getUser(player);
 		McMMOPlayer mcmmoplayer = UserManager.getPlayer(player);
-		String skill = e.getSkill().toString().toLowerCase();
-		int skill_level = e.getSkillLevel();
+		PlayerProfile mcmmoprofile = mcmmoplayer.getProfile();
 		FileConfiguration file = plugin.getConfig();
-
+		
+		String[] skillTypes = {"EXCAVATION", "FISHING", "HERBALISM", "MINING", "WOODCUTTING", "AXES", "ARCHERY", "SWORDS", "TAMING", "UNARMED", "ACROBATICS", "REPAIR"};
+		
 		//if (skill_level % 50 == 0) // For efficiency purposes, comment out for testing.
 		//{
+			
+		
 			for(String group : file.getConfigurationSection("groups").getKeys(false))
 			{
+				int total = 0;
+				int win = 0;
+				
 				for(String configSkill : file.getConfigurationSection("groups." + group + ".skills").getKeys(false))
 				{
-					int configSkillLevel = file.getInt("groups." + group + ".skills." + configSkill);
-					if (configSkill.equals(skill) && skill_level >= configSkillLevel)
+					for (String skillType : skillTypes)
 					{
-						player.sendMessage(ChatColor.AQUA + "You are eligible for the group " + group + " with " + skill + " level " + configSkillLevel + "!");
+						if (configSkill.toLowerCase().equals(skillType.toLowerCase()))
+						{
+							int skillLevel = mcmmoprofile.getSkillLevel(SkillType.valueOf(skillType));
+							int configSkillLevel = file.getInt("groups." + group + ".skills." + configSkill);
+							
+							//player.sendMessage(ChatColor.AQUA + "Checking skill level " + skillType + ": " + skillLevel + "...");
+							
+							if (skillLevel >= configSkillLevel)
+							{
+								win++;
+								//player.sendMessage(ChatColor.AQUA + skillType + " WIN:  " + skillLevel + "...");
+							}
+						}
+					}
+					total++;
+				}
+				
+				if (win == total)
+				{
+					player.sendMessage(ChatColor.AQUA + "You are eligible for the " + group + " group!");
+					
+					int exist = 0;
+					for(PermissionGroup pexgroup : peplayer.getGroups())
+					{
+						if (pexgroup.getName().equals(group))
+						{
+							exist++;
+						}
+					}
+					
+					if (exist == 0)
+					{
+						peplayer.addGroup(group);
+						player.sendMessage(ChatColor.RED + "You've been added to the group " + group + "!");
 					}
 				}
 			}
